a2d85820f6fa0466bc804bd4bf4ed4d7689e0732
stevensc07 <stevensc@uninorte.edu.co>
2021-05-02 23:15:17 -0500

    MoviesTime-App 100%

--- src/view/public/home/Information.js
+++ src/view/public/home/Information.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import logo from '../../../assets/img/logo.png'
 import userIcon from '../../../assets/img/user-icon.png';
+import play from '../../../assets/img/play.png';
 import './__styles__/index.css';
 import Movie from "../../../components/Movie";
 import { Link } from 'react-router-dom'
+import { Rating } from '@material-ui/lab'
 const Page = (props) => {
+    const setVoteClass = (vote) => {
+        if (vote >= 8) {
+            return "green";
+        } else if (vote >= 6) {
+            return "orange";
+        } else {
+            return "red";
+        }
+    };
     console.log('props son ', props.location.aboutprops)
     return (
         <>
@@ -30,19 +41,29 @@ const Page = (props) => {
 
             </header>
 
-            <div style={{ display: 'flex', flexDirection: 'row' , justifyContent:'center'}}>
+            <div style={{ display: 'flex', flexDirection: 'row', paddingTop:50, }}>
                 <div >
                     <Movie key={props.location.aboutprops.movie.id} {...props.location.aboutprops.movie} />
                 </div>
                 <div className="card" >
                     <section >
                         <div>
+
                             <h2 className="info__title">{props.location.aboutprops.movie.original_title}</h2>
-                            <h6 >{props.location.aboutprops.movie.release_date} </h6>
+                            <div style={{ display: 'flex', marginTop: 20, width: '1%' }}>
+                                <Rating size="small" readOnly={true} name="customized-empty" precision={0.5} defaultValue={props.location.aboutprops.movie.vote_average} max={10} />
+
+                                <span className={`tag ${setVoteClass(props.location.aboutprops.movie.vote_average)}`}>{props.location.aboutprops.movie.vote_average}</span>
+                            </div>
+
+                            <h6 style={{ display: 'flex', marginTop: -10 }}>{props.location.aboutprops.movie.release_date} </h6>
 
                         </div>
                         <div style={{ width: '85%' }} >
-                            <samp  className='font' > {props.location.aboutprops.movie.overview}</samp>
+                            <samp className='font' > {props.location.aboutprops.movie.overview}</samp>
+                        </div>
+                        <div style={{ marginLeft: '-3%' }}>
+                            <img className="play__img" src={play} alt="MoviesTime" />
                         </div>
 
                     </section>
